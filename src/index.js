8d550f17f41b228fc9de29056702d4b1d0e36e35
VeronicaZav <96069778+VeronicaZav@users.noreply.github.com>
2022-01-28 13:07:13 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const pairs = new Map(bracketsConfig)
+  let arr = []
+  const a = () => arr.length === 0
+  for (let char of str) {
+    if (pairs.has(char) && pairs.get(char) !== char) {
+      arr.push(char)
+    } 
+    else {
+      let top = arr[arr.length - 1]
+      if (pairs.get(top) === char) {
+        arr.pop()
+      } 
+      else {
+        arr.push(char)
+      }
+    }
+  }
+  return a()
 }
